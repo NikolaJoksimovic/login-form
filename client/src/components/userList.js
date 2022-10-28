f7446e916e3b3c330414e6fc608c06cae8134dde
joksa19 <nikolajoksimovic95@gmail.com>
2022-10-28 12:31:37 +0200

    add so the list of users appear on frontend

--- client/src/components/userList.js
+++ client/src/components/userList.js
@@ -1,14 +1,26 @@
 import React from "react";
 import axios from "axios";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
 const UserList = ({ users }) => {
+  const [backendData, setBackendData] = useState([]);
+
   useEffect(() => {
     axios.get("./home").then((response) => {
-      console.log(response.data);
+      setBackendData(response.data.users);
     });
   }, []);
-  return <div>show all users</div>;
+
+  return (
+    <div>
+      <ul>
+        {backendData.map((item) => {
+          return <li key={item._id}>{item.username}</li>;
+        })}
+      </ul>
+      <hr />
+    </div>
+  );
 };
 
 export default UserList;
